c2c9b41647ea588610d96e358f8614152eee0f79
billdevmaster71 <billdevmaster@gmail.com>
2022-10-28 20:55:29 +0900

    fix: update the time server

--- app/Http/Controllers/Frontend/HomePageController.php
+++ app/Http/Controllers/Frontend/HomePageController.php
@@ -245,6 +245,7 @@ class HomePageController extends Controller
     public function booking(Request $request) {
         $location = Locations::find($request['office']);
         $ret_data = [];
+        $date1 = date_create($request['start_date']);
         $ret_data['office'] = [];
         $ret_data['days'] = [];
         $ret_data['office']['allow_brn_max_time'] = '0';
@@ -252,10 +253,9 @@ class HomePageController extends Controller
         $ret_data['office']['brn_min_time'] = '60';
         $ret_data['office']['slot_length'] = $location->interval;
         $day = [];
-        if ($date1 <= date_create("2022-10-30")) {
+        $day['date'] = strtotime($request['start_date']) * 1 - 3600;
+        if ($date1 > date_create("2022-10-30")) {
             $day['date'] = strtotime($request['start_date']) * 1 - 7200;
-        } else {
-            $day['date'] = strtotime($request['start_date']) * 1 - 3600;
         }
         $day['openTimes'] = $this->getLocationOpenTimes($request['office'], $request['start_date']);
         $day['resources'] = $this->getLocationPesuboxs($request['office'], $request['start_date']);
